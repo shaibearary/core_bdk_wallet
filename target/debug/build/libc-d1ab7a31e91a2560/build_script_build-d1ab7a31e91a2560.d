3bb2a3c380c9dd0e904bdb69b9be3196a9b1364c
Sherry <26760878+shaibearary@users.noreply.github.com>
2025-03-15 14:51:31 +0800

    Remove target directory from version control and add to .gitignore

--- target/debug/build/libc-d1ab7a31e91a2560/build_script_build-d1ab7a31e91a2560.d
+++
@@ -1,5 +0,0 @@
-/Users/apple/core_bdk_wallet/target/debug/build/libc-d1ab7a31e91a2560/build_script_build-d1ab7a31e91a2560: /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.166/build.rs
-
-/Users/apple/core_bdk_wallet/target/debug/build/libc-d1ab7a31e91a2560/build_script_build-d1ab7a31e91a2560.d: /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.166/build.rs
-
-/Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.166/build.rs:
--- target/debug/deps/pin_utils-096ea283e33a9616.d
+++
@@ -1,9 +0,0 @@
-/Users/apple/core_bdk_wallet/target/debug/deps/libpin_utils-096ea283e33a9616.rmeta: /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/lib.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/stack_pin.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/projection.rs
-
-/Users/apple/core_bdk_wallet/target/debug/deps/libpin_utils-096ea283e33a9616.rlib: /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/lib.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/stack_pin.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/projection.rs
-
-/Users/apple/core_bdk_wallet/target/debug/deps/pin_utils-096ea283e33a9616.d: /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/lib.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/stack_pin.rs /Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/projection.rs
-
-/Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/lib.rs:
-/Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/stack_pin.rs:
-/Users/apple/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-utils-0.1.0/src/projection.rs:
